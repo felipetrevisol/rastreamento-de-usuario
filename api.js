6b66c192797a8f19fbefcaa0620b9c7e80a50395
Felipe Trevisol <felipetrevisol@gmail.com>
2017-08-15 18:18:37 -0300

    Api Javascript para rastrear usuários.

---
+++ api.js
@@ -0,0 +1,44 @@
+/**
+	This represent de Client Page Access.
+*/
+var ClientPageAccess = {
+
+	timestamp:new Date(),
+	hostname:location.hostname,
+	pathname:location.pathname,
+	
+	email:function () {
+		return document.cookie;
+	},
+
+	toHttpBody:function () {
+		return JSON.stringify(ClientPageAccess, function(key, value) {
+			var isEmailField = key === 'email' && typeof value === 'function';
+			if (isEmailField) value = value();
+			return value;
+		});
+	},
+
+	registerInRDStation:function () {
+		var request = new XMLHttpRequest();
+		{
+			var asynchronous = true;
+			request.open('POST', 'localhost:8080/api/cadastrar-acesso-do-usuario', asynchronous);
+			request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+			request.send(ClientPageAccess.toHttpBody());
+		}
+	}
+};
+
+
+
+var UserIndentifier = {
+	expires:new Date(),
+	email:"felipe@gmail.com"
+};
+
+
+
+window.onload = function () {
+	ClientPageAccess.registerInRDStation();
+};
